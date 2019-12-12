be1fcec1acc2028f1fd1ea08cbeafa532bd93213
hientuminh <hientuminh@gmail.com>
2019-12-13 06:04:18 +0700

    Add message resouces

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(username: "admin", password: "admin")
+User.create(username: "hientu", password: "hientu")
+User.create(username: "yennhi", password: "yennhi")
+
+Message.create(body: "Hello", user_id: 2)
+Message.create(body: "Good morning,", user_id: 3)
+Message.create(body: "Have a nice day", user_id: 2)
